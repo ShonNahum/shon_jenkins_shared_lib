78905d691fb55c2b81db779b0be0a706dca2da26
ShonNahum <shon304082928@gmail.com>
2025-06-02 21:47:25 +0300

    add server var

--- vars/server.groovy
+++ vars/server.groovy
@@ -1,1 +1,12 @@
-def call()
+def call() {
+    pipeline {
+        agent any
+        stages {
+            stage('Init') {
+                steps {
+                    echo "Running from shared library!"
+                }
+            }
+        }
+    }
+}
