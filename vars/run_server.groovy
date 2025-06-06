74ca320fe07dce2111e0b74f4e0707eb63add899
ShonNahum <shon304082928@gmail.com>
2025-06-06 18:24:39 +0300

    added docker

--- vars/run_server.groovy
+++ vars/run_server.groovy
@@ -1,12 +1,11 @@
 def call() {
   pipeline {
     agent {
-      docker {
+        docker {
         image 'python:3.10'
         args '-u root:root'
-      }
+        }
     }
-
     environment {
       IMAGE_NAME = "shonnahum/sm:${env.BUILD_NUMBER}"
     }
