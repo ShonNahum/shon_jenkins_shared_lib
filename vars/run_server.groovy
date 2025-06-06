e657b90b4a8e073a25a849675e704905d611036d
ShonNahum <shon304082928@gmail.com>
2025-06-06 19:26:23 +0300

    fix server

--- vars/run_server.groovy
+++ vars/run_server.groovy
@@ -1,23 +1,22 @@
 def call() {
     node {
+        def imageName = "shonnahum/sm:${env.BUILD_NUMBER}"
+
         stage('Checkout') {
             checkout scm
-            sh '''
-            export PATH=$PATH:/usr/bin
-            docker --version
-            docker build -t myimage .
-            '''
-
         }
+
         stage('Build Docker Image') {
-            def imageName = "shonnahum/sm:${env.BUILD_NUMBER}"
+            sh 'docker --version'
             sh "docker build -t ${imageName} ."
         }
+
         stage('Login to Artifactory') {
             withCredentials([usernamePassword(credentialsId: 'artifactory-creds-id', usernameVariable: 'ART_USER', passwordVariable: 'ART_PASS')]) {
-                sh "docker login -u $ART_USER -p $ART_PASS "
+                sh "docker login -u $ART_USER -p $ART_PASS"
             }
         }
+
         stage('Push Docker Image') {
             sh "docker push ${imageName}"
         }
