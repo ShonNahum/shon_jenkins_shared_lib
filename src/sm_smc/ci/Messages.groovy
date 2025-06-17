d201bc6f1935be4318a5c6908c547f2b34221ac0
ShonNahum <shon304082928@gmail.com>
2025-06-17 15:50:14 +0300

    add scm

--- src/sm_smc/ci/Messages.groovy
+++ src/sm_smc/ci/Messages.groovy
@@ -2,7 +2,7 @@ package sm_smc.ci
 
 class Messages {
     static void start(script) {
-        script.echo "***************" Starting CI ***************" "
+        script.echo "***************" Starting CI ***************"
     }
     static void checkOut(script) {
         script.echo "***************" Checking out The Project ***************"
